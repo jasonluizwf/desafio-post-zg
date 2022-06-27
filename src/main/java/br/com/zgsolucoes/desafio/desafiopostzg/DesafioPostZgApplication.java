c658e25dc81eafcec85a04625e65aff3c6099f74
Victor Mota <68482280+jasonluizwf@users.noreply.github.com>
2022-06-27 12:21:44 -0300

    Iniciando desafio Post ZG

---
+++ src/main/java/br/com/zgsolucoes/desafio/desafiopostzg/DesafioPostZgApplication.java
@@ -0,0 +1,13 @@
+package br.com.zgsolucoes.desafio.desafiopostzg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPostZgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPostZgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zgsolucoes/desafio/desafiopostzg/DesafioPostZgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zgsolucoes.desafio.desafiopostzg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPostZgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
